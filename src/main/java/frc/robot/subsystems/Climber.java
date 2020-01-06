347d6d22af13555724f8d974ea4348388ae24288
William Meathrel <wcmeathrel@gmail.com>
2020-01-06 16:25:37 -0500

    Extend Subsystembase
    Classes now extend Subsystembase

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
-public class Climber{
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase{
     public static Climber m_instance = null;
 
     /**
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
-public class Intake{
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase{
     public static Intake m_instance = null;
 
     /**
