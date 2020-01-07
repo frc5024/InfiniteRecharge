4a387d1c084f3d7efc376742e3e6a28e47c9e020
Evan Pratten <ewpratten@gmail.com>
2020-01-07 14:24:44 -0500

    added fieldsim py file

---
+++ src/main/python/fieldsim/fieldsim.py
@@ -0,0 +1,11 @@
+import pygame
+import pynetworktables
+print("Starting fieldsim")
+
+# Init pygame
+pygame.init()
+gameDisplay = pygame.display.set_mode((1228,635))
+pygame.display.set_caption('5024 Fieldsim')
+
+# Colors
+blue = (0,0,180)
