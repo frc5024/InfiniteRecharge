28962febb1a319302b43fc8ced782805fcc28095
Evan Pratten <ewpratten@gmail.com>
2020-01-09 19:54:02 -0500

    End of day 2 cleanup

--- src/main/java/frc/robot/autonomous/Chooser.java
+++ src/main/java/frc/robot/autonomous/Chooser.java
@@ -95,7 +95,16 @@ public class Chooser {
                 new EasyTrajectory(new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(2, 0, Rotation2d.fromDegrees(0))),
                 new SpeedConstraint(1.0, 1.0)));
 
-        outputCommand.addCommands(PathGenerator.generateInPlaceRotation(90, 2.0));
+        outputCommand.addCommands(PathGenerator.generateInPlaceRotation(45, 2.0));
+
+        outputCommand.addCommands(PathGenerator.generate(
+                new EasyTrajectory(new Pose2d(2, 0, new Rotation2d(45)), new Pose2d(3, 1, Rotation2d.fromDegrees(45))),
+                new SpeedConstraint(1.0, 1.0)));
+
+        // outputCommand.addCommands(PathGenerator.generate(
+        // new EasyTrajectory(new Pose2d(2, 0, new Rotation2d(45)), new Pose2d(3, 0.5,
+        // Rotation2d.fromDegrees(45))),
+        // new SpeedConstraint(1.0, 1.0)));
 
         // /* Start building command based on params */
 
