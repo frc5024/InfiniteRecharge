72d4006840125c7332cbf1e0e9b92ad3d7c5af67
Evan Pratten <ewpratten@gmail.com>
2020-01-13 15:15:33 -0500

    fix auton startpoints to be correct to field coords

--- src/main/java/frc/robot/autonomous/AutonomousStartpoints.java
+++ src/main/java/frc/robot/autonomous/AutonomousStartpoints.java
@@ -23,12 +23,17 @@ public class AutonomousStartpoints {
      * side's sector line, against the right wall, facing towards the centre line of
      * the field.
      */
-    public static final Pose2d SECTOR_LINE_RIGHT = new Pose2d(Units.inchesToMeters(120) - RobotConstants.DriveTrain.Measurements.DRIVEBASE_WIDTH / 2,
-    ((FieldConstants.FIELD_WIDTH / 2) - RobotConstants.DriveTrain.Measurements.DRIVEBASE_LENGTH / 2)*-1,
-    Rotation2d.fromDegrees(90));
-    //new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
-    //  new Pose2d(
-    //         FieldConstants.ALLIANCE_WALL_TO_FAR_SECTOR + (RobotConstants.DriveTrain.Measurements.DRIVEBASE_WIDTH / 2),
-    //         ((FieldConstants.FIELD_WIDTH / 2) - (RobotConstants.DriveTrain.Measurements.DRIVEBASE_LENGTH / 2)),
-    //         Rotation2d.fromDegrees(-90.0));
+    public static final Pose2d SECTOR_LINE_RIGHT = new Pose2d(
+            Units.inchesToMeters(120) - (RobotConstants.DriveTrain.Measurements.DRIVEBASE_LENGTH / 2)
+                    - Units.inchesToMeters(8),
+            ((FieldConstants.FIELD_WIDTH / 2) - RobotConstants.DriveTrain.Measurements.DRIVEBASE_WIDTH / 2
+                    - Units.inchesToMeters(14)) * -1,
+            Rotation2d.fromDegrees(0));
+    // new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
+    // new Pose2d(
+    // FieldConstants.ALLIANCE_WALL_TO_FAR_SECTOR +
+    // (RobotConstants.DriveTrain.Measurements.DRIVEBASE_WIDTH / 2),
+    // ((FieldConstants.FIELD_WIDTH / 2) -
+    // (RobotConstants.DriveTrain.Measurements.DRIVEBASE_LENGTH / 2)),
+    // Rotation2d.fromDegrees(-90.0));
 }
