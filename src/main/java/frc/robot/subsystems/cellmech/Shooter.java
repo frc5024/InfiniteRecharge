bf8776a72d04d1831717e9d08c0f12f3340eee9b
Evan Pratten <ewpratten@gmail.com>
2020-01-17 14:13:02 -0500

    Added shooter logic

--- src/main/java/frc/robot/subsystems/cellmech/Shooter.java
+++ src/main/java/frc/robot/subsystems/cellmech/Shooter.java
@@ -3,8 +3,12 @@ package frc.robot.subsystems.cellmech;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib5k.control.JRADController;
+import frc.lib5k.utils.Mathutils;
+import frc.robot.RobotConstants;
 
 /**
  * Robot Shooter subsystem
@@ -12,14 +16,53 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Shooter extends SubsystemBase {
     public static Shooter s_instance = null;
 
-    private static final double DEFAULT_RPM_EPSILON = 50;
+    /**
+     * Shooter motor controller
+     */
+    private WPI_TalonSRX m_motorController = new WPI_TalonSRX(RobotConstants.Shooter.MOTOR_ID);
 
-    private WPI_TalonSRX m_tempTalon = new WPI_TalonSRX(5);
+    /**
+     * System states
+     */
+    private enum SystemState {
+        IDLE, // System Idle
+        SPIN_UP, // Flywheel spinning up
+        SPIN_DOWN, // Flywheel spinning down
+        HOLD, // Flywheel holding a speed
+        MANUAL, // Manual voltage control
+        UNJAM, // Flywheel unjamming
+
+    }
+
+    /**
+     * Tracker for shooter system state.
+     */
+    private SystemState m_systemState = SystemState.IDLE;
+    private SystemState m_lastState = null;
+
+    // Output value. Depending on mode, this will become different things
+    private double output = 0.0;
+
+    /**
+     * Spin-up PID controller
+     */
+    private PIDController m_spinupController;
+
+    /**
+     * Flywheel hold controller
+     */
+    private JRADController m_holdController;
 
     private Shooter() {
 
-        m_tempTalon.enableVoltageCompensation(true);
-        m_tempTalon.setNeutralMode(NeutralMode.Coast);
+        // Disable motor brakes
+        m_motorController.setNeutralMode(NeutralMode.Coast);
+
+        // Configure the rpm controllers
+        m_spinupController = new PIDController(RobotConstants.Shooter.kPVel, RobotConstants.Shooter.kIVel,
+                RobotConstants.Shooter.kDVel);
+        m_holdController = new JRADController(RobotConstants.Shooter.kJ, RobotConstants.Shooter.kF,
+                RobotConstants.Shooter.kLoadRatio);
     }
 
     /**
@@ -38,19 +81,169 @@ public class Shooter extends SubsystemBase {
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Shooter Voltage", m_tempTalon.getMotorOutputVoltage());
-    }
-    
-    public void setVoltage(double voltage) {
-        
+        // Publish controller voltage
+        SmartDashboard.putNumber("Shooter Voltage", m_motorController.getMotorOutputVoltage());
+
+        // Determine if this state is new
+        boolean isNewState = false;
+        if (m_systemState != m_lastState) {
+            isNewState = true;
+        }
+
+        /* Handle states */
+        switch (m_systemState) {
+        case IDLE:
+
+            // Handle the idle state
+            handleIdle(isNewState);
+            break;
+
+        case SPIN_UP:
+
+            // Handle the spinup state
+            handleSpinUp(isNewState);
+            break;
+
+        case SPIN_DOWN:
+
+            // Handle the spindown state
+            handleSpinDown(isNewState);
+            break;
+
+        case HOLD:
+
+            // Handle holding motor at velocity
+            handleHold(isNewState);
+            break;
+
+        case UNJAM:
+
+            // TODO: Unjam code
+            break;
+
+        default:
+
+            // Set the system to spin down in case it is spinning and loses track of state.
+            m_systemState = SystemState.SPIN_DOWN;
+
+        }
+
+        // Set the last state
+        m_lastState = m_systemState;
     }
 
-    public void setRPM(double rmp) {
-        setRPM(rmp, DEFAULT_RPM_EPSILON);
+    /**
+     * Handle system idle state
+     * 
+     * @param newState Is this state new?
+     */
+    private void handleIdle(boolean newState) {
+
+        if (newState) {
+
+            // Enable voltage compensation for use during "test mode"
+            m_motorController.enableVoltageCompensation(true);
+
+            // Force-set the motor to 0.0V
+            m_motorController.set(0.0);
+
+            // Force-set output
+            output = 0.0;
+        }
+
     }
 
-    public void setRPM(double rpm, double epsilon) {
-        
+    /**
+     * Handle flywheel spinup using PID control to get it near speed
+     * 
+     * @param newState Is this state new?
+     */
+    private void handleSpinUp(boolean newState) {
+
+        if (newState) {
+            // Disable voltage compensation to allow velocity controllers to take control
+            m_motorController.enableVoltageCompensation(true);
+
+            // Configure the spinup controller
+            m_spinupController.reset();
+            m_spinupController.setTolerance(RobotConstants.Shooter.VOLTAGE_EPSILON);
+        }
+
+        // Get the current motor output voltage
+        double voltage = m_motorController.getMotorOutputVoltage();
+
+        // Calculate the motor output
+        double motorOutput = m_spinupController.calculate(voltage, this.output);
+
+        // Disallow reverse motor output
+        motorOutput = Mathutils.clamp(motorOutput, 0, 12);
+
+        // Set the motor output
+        m_motorController.setVoltage(motorOutput);
+
+        // Switch to HOLD state if spinup complete
+        if (m_spinupController.atSetpoint()) {
+
+            // Move to next state
+            m_systemState = SystemState.HOLD;
+        }
+    }
+
+    /**
+     * Handle flywheel spindown to 0
+     * 
+     * @param newState Is this state new?
+     */
+    private void handleSpinDown(boolean newState) {
+
+        // TODO: Use encoder to detect velocity, and do a smooth spindown
+        m_motorController.set(0);
+
+    }
+
+    /**
+     * Handle holding the flywheel velocity
+     * 
+     * @param newState Is this state new?
+     */
+    private void handleHold(boolean newState) {
+
+        if (newState) {
+
+            // Set the JRAD setpoint
+            m_holdController.setSetpoint(this.output);
+        }
+
+        // Get the current motor output voltage
+        double voltage = m_motorController.getMotorOutputVoltage();
+
+        // Calculate the motor output
+        double motorOutput = m_holdController.calculate(voltage);
+
+        // Disallow reverse motor output
+        motorOutput = Mathutils.clamp(motorOutput, 0, 12);
+
+        // Set the motor output
+        m_motorController.setVoltage(motorOutput);
+
+    }
+
+    public void setOutputPercent(double val) {
+
+        // Switch to "Spin Up" mode. If we are already at, or above the setpoint, it
+        // will automatically move to the next state.
+        m_systemState = SystemState.SPIN_UP;
+
+        // Set the desired voltage
+        output = val * RobotConstants.Shooter.MAX_VOLTAGE;
+
+    }
+
+    public void stop() {
+
+        // Set the mode to spin down
+        m_systemState = SystemState.SPIN_DOWN;
+
     }
 
 }
