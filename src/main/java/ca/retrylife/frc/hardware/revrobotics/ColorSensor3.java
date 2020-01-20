1819000931616adef45f6afb28b97040f63462e9
Evan Pratten <ewpratten@gmail.com>
2020-01-20 10:28:56 -0500

    idk

---
+++ src/main/java/ca/retrylife/frc/hardware/revrobotics/ColorSensor3.java
@@ -0,0 +1,18 @@
+package ca.retrylife.frc.hardware.revrobotics;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+public class ColorSensor3 {
+
+    public void configure() {
+
+    }
+
+    public double getProximity() {
+        return 0.0;
+    }
+
+    public Color getColor() {
+        return null;
+    }
+}
