010e78f34f8de1cdf60f9794f9249e1f8dc0e351
Samuel <slownie16@gmail.com>
2020-01-23 17:14:15 -0500

    committing

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 import frc.robot.RobotConstants;
 
@@ -16,8 +17,20 @@ public class Climber extends SubsystemBase {
      */
     private Servo m_climbServo;
 
+    /**
+     * Hall effect sensors on the climbers
+     */
+    private DigitalInput m_lowHalleffect;
+    private DigitalInput m_highHalleffect;
+
     private Climber() {
+    
+        /*Climb Servo */
         m_climbServo = new Servo(RobotConstants.Climber.CLIMBER_TALON);
+    
+        /*Low and High Halleffect sensors */
+        m_lowHalleffect = new DigitalInput(RobotConstants.Climber.LOW_HALLEFFECT);
+        m_highHalleffect = new DigitalInput(RobotConstants.Climber.HIGH_HALLEFFECT);
     }
 
     /**
@@ -34,4 +47,21 @@ public class Climber extends SubsystemBase {
 
     }
 
+    public void lowClimb() {
+        /**
+         * If the lower hall effect sensor gets triggered
+         */
+        if (m_lowHalleffect.get()) {
+
+        }
+    }
+
+    public void highClimb() {
+        /**
+         * If the higher hall effect sensor gets triggered
+         */
+        if (m_highHalleffect.get()) {
+            
+        }
+    }
 }
