c3fb10a2a920b80c88a6ae423fa768ca85ccc5ee
Evan Pratten <ewpratten@gmail.com>
2020-02-02 19:37:00 -0500

    near pose finding

--- src/main/java/frc/lib5k/kinematics/purepursuit/Follower.java
+++ src/main/java/frc/lib5k/kinematics/purepursuit/Follower.java
@@ -1,5 +1,80 @@
 package frc.lib5k.kinematics.purepursuit;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import frc.lib5k.kinematics.statespace.models.DrivebaseState;
+
 public class Follower {
-    
+
+    // Path to follow
+    private Path m_path;
+
+    // Lookahead settings
+    private double m_lookaheadDist, m_lookaheadGain;
+    private Integer m_lastLookaheadIndex = null;
+
+    // Drivebase info
+    private double m_drivebaseWidth;
+    private Pose2d m_lastPose = null;
+
+    /**
+     * Create a path follower
+     * 
+     * @param path              Path to follow
+     * @param lookaheadDistance Lookahead distance
+     * @param lookaheadGain     Lookahead gain
+     * @param drivebaseWidth    Drivebase width
+     */
+    public Follower(Path path, double lookaheadDistance, double lookaheadGain, double drivebaseWidth) {
+        this.m_path = path;
+        this.m_lookaheadDist = lookaheadDistance;
+        this.m_lookaheadGain = lookaheadGain;
+        this.m_drivebaseWidth = drivebaseWidth;
+
+    }
+
+    /**
+     * Reset the follower
+     */
+    public void reset() {
+        m_lastLookaheadIndex = null;
+        m_lastPose = null;
+    }
+
+    public Translation2d getNextPoint(Pose2d robotPose) {
+
+        // Cast the pose up to a drivebase state
+        DrivebaseState state = (DrivebaseState) robotPose;
+        Translation2d stateRear = state.getRear(m_drivebaseWidth);
+
+        // Max nearest pose
+        Translation2d nearest = new Translation2d(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+        // Check if this is the first run
+        if (m_lastLookaheadIndex == null) {
+
+            // Search for the nearest pose
+            for (int i = 0; i < m_path.getPoses().length; i++) {
+
+                // Determine diff
+                double dx = stateRear.getX() - m_path.getPoses()[i].getX();
+                double dy = stateRear.getY() - m_path.getPoses()[i].getY();
+
+                // Determine distance
+                double d = Math.hypot(dx, dy);
+
+                // If d is closer than the nearest, we have found a new nearest point
+                if (d < Math.hypot(stateRear.getX() - nearest.getX(), stateRear.getY() - nearest.getY())) {
+
+                    // Set the nearest translation
+                    nearest = m_path.getPoses()[i];
+
+                    // Set the lookahead index
+                    m_lastLookaheadIndex = i;
+                }
+            }
+
+        }
+
+    }
 }
--- src/main/java/frc/lib5k/kinematics/purepursuit/Path.java
+++ src/main/java/frc/lib5k/kinematics/purepursuit/Path.java
@@ -1,8 +1,6 @@
 package frc.lib5k.kinematics.purepursuit;
 
 import java.util.ArrayList;
-
-import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.util.Units;
 
@@ -12,7 +10,7 @@ public class Path {
     private static final double POINT_SPACING = Units.inchesToMeters(6.0);
 
     // Path points
-    private ArrayList<Pose2d> m_points;
+    private ArrayList<Translation2d> m_points;
 
     /**
      * Create a motion path from points
@@ -20,7 +18,7 @@ public class Path {
      * @param smoothness Path smoothness (Should be between 0 and 1)
      * @param waypoints  Path waypoints to follow
      */
-    public Path(Pose2d... waypoints) {
+    public Path(Translation2d... waypoints) {
         this.m_points = new ArrayList<>();
 
         // Fill in extra points
@@ -28,8 +26,8 @@ public class Path {
         for (; i < waypoints.length - 1; i++) {
 
             // Get the start and end translations
-            Translation2d startTrans = waypoints[i].getTranslation();
-            Translation2d endTrans = waypoints[i + 1].getTranslation();
+            Translation2d startTrans = waypoints[i];
+            Translation2d endTrans = waypoints[i + 1];
 
             // Create a new Translation of the difference between poses
             Translation2d vector = new Translation2d(endTrans.getX() - startTrans.getX(),
@@ -44,13 +42,17 @@ public class Path {
 
             // Add inner points
             for (int j = 0; j < innerCount; j++) {
-                m_points.add(new Pose2d(new Translation2d((startTrans.getX() + (vector.getX() * j)),
-                        (startTrans.getY() + (vector.getY() * j))), waypoints[i].getRotation()));
+                m_points.add(new Translation2d((startTrans.getX() + (vector.getX() * j)),
+                        (startTrans.getY() + (vector.getY() * j))));
             }
         }
 
         // Add the last point to the points list
-        m_points.add(waypoints[i + 1]);
+        m_points.add(waypoints[i]);
+    }
+
+    public Translation2d[] getPoses() {
+        return m_points.toArray(new Translation2d[m_points.size()]);
     }
 
 }
