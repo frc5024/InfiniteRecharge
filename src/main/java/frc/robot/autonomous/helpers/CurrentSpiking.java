95c35b82eb6d1ec0a2eeb1dfa076f6990727578c
William Meathrel <wcmeathrel@gmail.com>
2020-02-27 18:34:05 -0500

    small steps

--- src/main/java/frc/robot/autonomous/helpers/CurrentSpiking.java
+++ src/main/java/frc/robot/autonomous/helpers/CurrentSpiking.java
@@ -7,16 +7,24 @@ public class CurrentSpiking{
 
     PowerDistributionPanel powerDistributionPanel = new PowerDistributionPanel(RobotConstants.Power.PDP_CAN_ID);
     private int channel;
+    private double targetSpike;
 
-    public CurrentSpiking(int channel){
+    public CurrentSpiking(int channel, double targetSpike){
         this.channel = channel;
+        this.targetSpike = targetSpike;
     }
 
     public double getChannelCurrent(int channel){ 
         return powerDistributionPanel.getCurrent(channel);
     }
 
-    public void setSpike(){}
+    public boolean hitTargetSpike(){
+        if(targetSpike >= getChannelCurrent(channel)){
+            return true;
+        }
+
+        return false;
+    }
 
 
 
