6a93bc0c357ab37afd6fc7a59f298ddc0af136a1
Evan Pratten <ewpratten@gmail.com>
2020-03-06 18:12:44 -0500

    Add SmartServo

---
+++ src/main/java/frc/lib5k/components/SmartServo.java
@@ -0,0 +1,11 @@
+package frc.lib5k.components;
+
+import edu.wpi.first.wpilibj.Servo;
+
+public class SmartServo extends Servo {
+
+    public SmartServo(int channel) {
+        super(channel);
+    }
+    
+}
